6c95ce015033caed920e24594b2793e6f8f77062
m305-43\w401 <zxsa3106666@gmail.com>
2018-03-15 18:05:57 +0800

    no message

---
+++ BMI計算機/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("輸入體重:");
+            double kg = double.Parse(Console.ReadLine());
+            Console.Write("輸入身高:");
+            double cm = double.Parse(Console.ReadLine());
+            double BMI = kg/((cm*0.01)*(cm * 0.01));
+            Console.Write("BMI值為:"+ BMI);
+            Console.ReadLine();
+        }
+    }
+}
